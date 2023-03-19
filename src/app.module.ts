5321bbc51477c029c3f12a5aae91ee1844955268
domscript <vladimirndsm@gmail.com>
2023-03-19 10:07:34 +0700

    mongoDB new password

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-
-const root =
-  'mongodb+srv://Dominic-projects:oSL62YldzUyQkRP7@cluster0.b52ql.mongodb.net/nestjs-demo?retryWrites=true&w=majority';
+import { root } from '../CONST';
 
 @Module({
   imports: [ProductsModule, MongooseModule.forRoot(root)],
